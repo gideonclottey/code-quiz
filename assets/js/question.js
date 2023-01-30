fec0992f5ea27e2e0d94eb20dc46dc54e287eb92
gideonclottey <gideonclotteyint@gmail.com>
2023-01-30 21:10:31 +0000

    starter files added and to worked on

---
+++ assets/js/question.js
@@ -0,0 +1,13 @@
+var questions = [
+    {
+        title:"commonly used data types do not include:",
+        choices:["string ", "booleans","alerts", "numbers"],
+        answer:"alerts"
+
+    },
+    {
+        title:"The condition in an if / else statement is encloded ",
+        choices:["quotes ", "curly brcases","arentheses", "square"],
+        answer:"parentheses"
+    }
+]
